37c68c400cdb3ff41437e279b0f645be5397443f
Joseph Diza <josephm.diza@gmail.com>
2022-12-04 05:34:20 -0800

    Move gtk widgets into widgets/gtk directory

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,2 +1,12 @@
-pub mod application;
-pub mod window;
+//use std::cfg;
+
+//#[cfg(target_os = "linux")]
+
+//if cfg!(linux) {
+//pub mod application;
+
+////#[cfg(target_os = "linux")]
+//pub mod window;
+//}
+
+pub mod gtk;
