544bf54a7577170f1f8b199e4f9dff14716188cd
Joseph Diza <josephm.diza@gmail.com>
2023-02-18 12:05:41 -0800

    Complete FileCard widget factory

--- src/main.rs
+++ src/main.rs
@@ -1,147 +1,107 @@
 use std::path::PathBuf;
 
-use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt, OrientableExt};
+use gtk::prelude::{
+    BoxExt,
+    ButtonExt,
+    GtkWindowExt,
+    OrientableExt
+};
+
 use relm4::{gtk,
-    Controller,
     ComponentParts,
     ComponentSender,
     RelmApp,
     RelmWidgetExt,
     SimpleComponent,
-    Component,
-    factory::{FactorySender, FactoryVecDeque, FactoryView}
+    factory::{FactorySender, FactoryVecDeque},
+    prelude::{DynamicIndex, FactoryComponent}
 };
 
-/*
- * 1. Read a directory of files into a string
- */
+// File Card Factory
 
-pub struct FileState {
-    pub full_path: String,
+#[derive(Debug)]
+struct FileCard {
+    pub full_path: PathBuf,
 }
 
-pub struct FileCard {
-    settings: FileState,
+#[derive(Debug)]
+enum FileCardMsg {
+    //Set(PathBuf),
+}
+
+#[derive(Debug)]
+enum FileCardOutput {
 }
 
 // FileCardFactory
 
-#[derive(Debug)]
-struct FileCardFactoryWidgets {
-    label: gtk::Label,
-}
-
-impl FactoryPrototype for FileCard {
-    type Factory = FactoryVecDeque<Self>;
-    type Widgets = FileCardFactoryWidgets;
-    type Root = gtk::Label;
-    type View = gtk::Box;
-    type Msg = AppMsg;
-
-    fn generate(&self, index: &usize, sender: Sender<AppMsg>) -> FileCardFactoryWidgets {
-        let button = gtk::Button::with_label(&self.value.to_string());
-        //let label = gtk::Label::new(Some(self.full_path.to_str().unwrap()))
-        let index = *index;
-        button.connect_clicked(move |_| {
-            sender.send(AppMsg::Clicked(index)).unwrap();
-        });
-
-        FileCardFactoryWidgets { label: button }
-    }
-
-    fn position(&self, _index: &usize) {}
-
-    fn update(&self, _index: &usize, widgets: &FileCardFactoryWidgets) {
-        widgets.label.set_label(&self.full_path.to_string());
-    }
-
-    fn get_root(widgets: &FileCardFactoryWidgets) -> &gtk::Label {
-        &widgets.label
-    }
-}
-
-/*
-#[relm4::component(pub)]
-impl SimpleComponent for FileCard {
-    type Widgets = DirectoryWidgets;
-    type Init = FileState;
-    type Input = FileMsg;
-    type Output = FileResponse;
+#[relm4::factory]
+impl FactoryComponent for FileCard {
+    type Init = PathBuf;
+    type Input = FileCardMsg;
+    type Output = FileCardOutput;
+    type CommandOutput = ();
+    type ParentInput = AppMsg;
+    type ParentWidget = gtk::Box;
 
     view! {
-        #[wrap(Some)]
-        gtk::Box {
-            gtk::Box {
-                gtk::Label {
-                    set_label: "Filename"
-                }
-            }
+        root = gtk::Box {
+            set_orientation: gtk::Orientation::Horizontal,
+            set_spacing: 10,
+
+            #[name(label)]
+            gtk::Label {
+                #[watch]
+                set_label: self.full_path.to_str().unwrap(),
+                set_width_chars: 32,
+            },
+
+            //#[name(add_button)]
+            //gtk::Button {
+                //set_label: "+",
+                //connect_clicked => FileCardMsg::Set,
+            //},
         }
     }
-
-    fn init(
-        settings: FileState,
-        root: &Self::Root,
-        sender: ComponentSender<Self>,
-    ) -> ComponentParts<Self> {
-        let model = FileCard {
-            settings,
-        };
-
-        // Make Treeview
-        //let view = gtk::TreeView::new();
-
-        //let file_column = gtk::TreeViewColumn::new();
-
-        //let icon_cell = gtk::CellRendererPixbuf::new();
-        //file_column.pack_start(&icon_cell, false);
-        //file_column.add_attribute(&icon_cell, "gicon", ICON_COLUMN.into());
-
-        //let filename_cell = gtk::CellRendererText::new();
-        //filename_cell
-            //.set_property("ellipsize", &pango::EllipsizeMode::End)
-            //.unwrap();
-        //file_column.pack_start(&filename_cell, true);
-        //file_column.add_attribute(&filename_cell, "text", NAME_COLUMN.into());
-
-        //let dir_expand_cell = gtk::CellRendererPixbuf::new();
-        //file_column.pack_end(&dir_expand_cell, false);
-        //file_column.add_attribute(&dir_expand_cell, "gicon", IS_DIR_COLUMN.into());
-
-        //view.append_column(&file_column);
-
-        //let store = list_store_for(&model.dir).unwrap();
-        //view.set_model(Some(&store));
-
-        //root.add(&view);
-
-        let widgets = view_output!();
-        ComponentParts { model, widgets }
+    fn init_model(full_path: Self::Init, _index: &DynamicIndex, _sender: FactorySender<Self>) -> Self {
+        Self { full_path }
     }
 
-    fn update(&mut self, input: FileMsg, sender: ComponentSender<Self>) {
-    }
+    //fn output_to_parent_input(output: Self::Output) -> Option<AppMsg> {
+        //Some(match output {
+            //CounterOutput::SendFront(index) => AppMsg::SendFront(index),
+            //CounterOutput::MoveUp(index) => AppMsg::MoveUp(index),
+            //CounterOutput::MoveDown(index) => AppMsg::MoveDown(index),
+        //})
+    //}
+
+    //fn update(&mut self, msg: Self::Input, _sender: FactorySender<Self>) {
+        //match msg {
+            //FileCardMsg::Set(path) => {
+                //self.full_path = path;
+            //}
+        //}
+    //}
 }
-*/
 
-struct AppModel {
+// Main App
+
+#[derive(Debug)]
+struct App {
     // For now only select one file at a time
     selected_file: Option<usize>,
-    //filesview: Controller<FileCard>,
     files: FactoryVecDeque<FileCard>,
 }
 
 #[derive(Debug)]
 enum AppMsg {
-    Add,
-    Remove,
-    Clicked(usize),
+    AddFileCard,
+    RemoveFileCard,
     Quit,
 }
 
 #[relm4::component]
-impl SimpleComponent for AppModel {
-    type Widgets = AppWidgets;
+impl SimpleComponent for App {
     type Init = u8;
     type Input = AppMsg;
     type Output = ();
@@ -152,11 +112,13 @@ impl SimpleComponent for AppModel {
             set_default_width: 640,
             set_default_height: 480,
 
+            // Main App View
             gtk::Box {
                 set_orientation: gtk::Orientation::Vertical,
                 set_spacing: 5,
                 set_margin_all: 5,
 
+                // Quit/Exit Button
                 gtk::Button {
                     set_label: "Quit",
                     // Emit quit signal
@@ -165,22 +127,45 @@ impl SimpleComponent for AppModel {
                     }
                 },
 
-                append = &gtk::Box {
-                    set_orientation: gtk::Orientation::Vertical,
-                    set_margin_all: 5,
-                    set_spacing: 5,
-                    factory!(model.files),
-                }
+                // Add/Remove file cards
+                gtk::Button {
+                    set_label: "Add file card",
+                    connect_clicked => AppMsg::AddFileCard,
+                },
 
-                //gtk::Label {
-                    //#[watch]
-                    //set_label: &format!("Initial Directory: {}", model.selected_path),
-                    //set_margin_all: 5,
-                //},
+                gtk::Button {
+                    set_label: "Remove file card",
+                    connect_clicked => AppMsg::RemoveFileCard,
+                },
+
+                // File Cards
+                #[local_ref]
+                files_box -> gtk::Box {
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_spacing: 5,
+                }
             }
         }
     }
 
+    /// Initialize the main application user interface.
+    fn init(
+        filecard: Self::Init,
+        root: &Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+
+        let filecards = FactoryVecDeque::new(gtk::Box::default(), sender.input_sender());
+        let model = App {
+            selected_file: None,
+            files: filecards
+        };
+
+        let files_box = model.files.widget();
+        let widgets = view_output!();
+
+        ComponentParts { model, widgets }
+    }
     //fn init(
         //root: Self::Init,
         //window: &Self::Root,
@@ -205,21 +190,11 @@ impl SimpleComponent for AppModel {
 
     fn update(&mut self, message: Self::Input, _sender: ComponentSender<Self>) {
         match message {
-            AppMsg::Add => {
-                self.files.push(FileCard {
-                    settings: FileState{
-                        full_path: PathBuf::from("aaaa"),
-                    },
-                });
-
+            AppMsg::AddFileCard => {
+                self.files.guard().push_back(PathBuf::from("bbbb"));
             }
-            AppMsg::Remove => {
-                self.files.pop();
-            }
-            AppMsg::Clicked(index) => {
-                if let Some(file_index) = self.files.get_mut(index) {
-                    println!("Clicked on {}", file_index);
-                }
+            AppMsg::RemoveFileCard => {
+                self.files.guard().pop_back();
             }
             AppMsg::Quit => {
                 // TODO: Close app on click
@@ -231,5 +206,5 @@ impl SimpleComponent for AppModel {
 
 fn main() {
     let app = RelmApp::new("io.github.jmdaemon.argo");
-    app.run::<AppModel>(0);
+    app.run::<App>(0);
 }
